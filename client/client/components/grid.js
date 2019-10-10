f6b2990f3bf7c7e8f76f42797bf3c87c03c45adf
Julie Thrall <juliethrallstewart@gmail.com>
2019-10-09 20:26:18 -0700

    installed and configured ag-grid, grid is displaying fetched data correctly

--- client/client/components/grid.js
+++ client/client/components/grid.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AgGridReact } from 'ag-grid-react';
+import '../node_modules/ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
-const Grid = () => {
+//The three lines above import the AgGridReact component, 
+//the grid "structure" stylesheet (ag-grid.css), and one of the 
+//available grid themes: (ag-theme-balham.css).
+
+
+
+const Grid = (props) => {
+
+    const [columns] = useState({
+        columnDefs: [{
+            headerName: "Subscribe Date", field: "subscribeDate"
+          }, {
+            headerName: "Id", field: "_id"
+          }, {
+            headerName: "Name", field: "name"
+
+          }, {
+            headerName: "Subscribed To Channel", field: "subscribedToChannel"
+          }, {
+            headerName: "Date", field: "date"
+          }],
+        //   rowData: [{
+        //     make: "Toyota", model: "Celica", price: 35000
+        //   }, {
+        //     make: "Ford", model: "Mondeo", price: 32000
+        //   }, {
+        //     make: "Porsche", model: "Boxter", price: 72000
+        //   }]
+        })
+    
+        const [row, setRow] = useState([])
+
+        useEffect(() => {
+            setRow(props.subscribers)
+        }, [])
+
     return (
         <>
         <p>Grid view of subscriber details</p>
+        <div 
+        className="ag-theme-balham"
+        style={{ 
+        height: '500px', 
+        width: '600px' }} 
+      >
+        <AgGridReact
+          columnDefs={columns.columnDefs}
+          rowData={row}>
+        </AgGridReact>
+        </div>
         </>
     )
 
--- client/client/pages/grid-page.js
+++ client/client/pages/grid-page.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Grid from '../components/grid'
 
 
-export default function GridPage () {
+export default function GridPage (props) {
     return (
         <Layout>
       <div>
           
         <p>This is the grid page</p>
+        <Grid subscribers={props.subscribers}/>
       </div>
       </Layout>
     );
   }
+
+  GridPage.getInitialProps = async function() {
+    const res = await fetch('http://localhost:3000/subscribers');
+    const data = await res.json();
+      console.log(data)
+    console.log(`Show data fetched. Count: ${data.length}`);
+  
+    return {
+      subscribers: data.map(subscriber => subscriber)
+    };
+  };
